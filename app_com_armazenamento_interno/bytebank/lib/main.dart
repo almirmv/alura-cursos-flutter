611dec4c28d443eea0a10715bd2b623647113620
almirmv <76816905+almirmv@users.noreply.github.com>
2022-05-15 20:48:23 -0300

    Commit aula 1
    código do final da aula 1.

---
+++ app_com_armazenamento_interno/bytebank/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Dashboard')),
+      ),
+    );
+  }
+}
