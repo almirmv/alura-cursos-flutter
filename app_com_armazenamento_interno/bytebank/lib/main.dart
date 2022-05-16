be745f91fefbcf405bbf78442e4e2534b2b9bff9
almirmv <76816905+almirmv@users.noreply.github.com>
2022-05-15 21:50:30 -0300

    commit aula 3
    código ao final da aula 3.

--- app_com_armazenamento_interno/bytebank/lib/main.dart
+++ app_com_armazenamento_interno/bytebank/lib/main.dart
@@ -10,21 +10,66 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard')),
-        body: Column(
-          children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              height: 120,
-              width: 100,
-              color: Colors.green,
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        appBarTheme: AppBarTheme(color: Colors.green[900]),
+        //primarySwatch não aceita Colors.green[900]. Somente Colors.green
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.blueAccent[700],
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          primary: Colors.blueAccent[700],
+        )),
+        buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.normal),
+      ),
+      home: const Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Theme.of(context).primaryColor,
               child: Column(
-                children: [Icon(Icons.people), Text('Contacts')],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    'Contacts',
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  )
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
