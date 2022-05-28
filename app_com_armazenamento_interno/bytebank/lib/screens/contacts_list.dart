a2a2466a13b0739d106165699e184b25980c15e4
almirmv <76816905+almirmv@users.noreply.github.com>
2022-05-28 18:27:40 -0300

    commit aula 8
    Configurando o builder do ListView.

--- app_com_armazenamento_interno/bytebank/lib/screens/contacts_list.dart
+++ app_com_armazenamento_interno/bytebank/lib/screens/contacts_list.dart
@@ -1,35 +1,23 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
-  const ContactsList({Key? key}) : super(key: key);
+  final List<Contact> contacts = List.empty(growable: true);
 
   @override
   Widget build(BuildContext context) {
+    contacts.add(Contact(0, 'Alex', 1000)); //testar interface...
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contacts'),
+        title: const Text('Contacts'),
       ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: Text(
-                'Alex',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -42,6 +30,30 @@ class ContactsList extends StatelessWidget {
               .then((newContact) => debugPrint(newContact.toString()));
         },
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact); //contrutor
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: const TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: const TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
