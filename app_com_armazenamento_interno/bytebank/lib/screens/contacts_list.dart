0998069fd093458c0181ab666a42439dccf0b2b0
almirmv <76816905+almirmv@users.noreply.github.com>
2022-05-28 19:02:59 -0300

    commit aula 10
    Carregando a lista de contatos. Já com correção para flutter 3.0 e "null exception"

--- app_com_armazenamento_interno/bytebank/lib/screens/contacts_list.dart
+++ app_com_armazenamento_interno/bytebank/lib/screens/contacts_list.dart
@@ -1,23 +1,37 @@
+import 'package:bytebank/database/app_database.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
-  final List<Contact> contacts = List.empty(growable: true);
+  const ContactsList({Key? key}) : super(key: key);
+
+  //final List<Contact> contacts = List.empty(growable: true);
 
   @override
   Widget build(BuildContext context) {
-    contacts.add(Contact(0, 'Alex', 1000)); //testar interface...
+    //contacts.add(Contact(0, 'Alex', 1000)); //testar interface...
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contacts'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Contact contact = contacts[index];
-          return _ContactItem(contact);
+      //indicando que o future vai receber uma <list<Contact>>
+      body: FutureBuilder<List<Contact>>(
+        future: findAll(), //faz busca dos contacts
+        builder: (context, snapshot) {
+          //checagem para evitar "null exception"
+          if (snapshot.hasData) {
+            final contacts = snapshot.data!;
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                final Contact contact = contacts[index];
+                return _ContactItem(contact);
+              },
+              itemCount: contacts.length,
+            );
+          }
+          return const Text('No contacts...');
         },
-        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
