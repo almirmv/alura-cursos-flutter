b7719fd0ae43d42631d14791fa72b349ae20845e
almirmv <76816905+almirmv@users.noreply.github.com>
2022-05-28 10:28:44 -0300

    commit aula 7 (correção)
    Correção do código da aula 7.

--- app_com_armazenamento_interno/bytebank/lib/database/app_database.dart
+++ app_com_armazenamento_interno/bytebank/lib/database/app_database.dart
@@ -2,13 +2,13 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 void createDatabase() {
-  //getdatabaepath retirna um Future que pegamos com then
+  //getdatabasepath retorna um Future<String> que pegamos com then
   getDatabasesPath().then((dbPath) {
     final String path = join(dbPath, 'bytebank.db');
     openDatabase(path, onCreate: (db, version) {
       db.execute('CREATE TABLE contacts('
           'id INTEGER PRIMARY KEY, '
-          'name TEXT'
+          'name TEXT, '
           'account_number INTEGER)');
     }, version: 1);
   });
