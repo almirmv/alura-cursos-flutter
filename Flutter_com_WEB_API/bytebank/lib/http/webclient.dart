0cbd77296c1555f5ecfe7f23493dfa8dad096168
almirmv <76816905+almirmv@users.noreply.github.com>
2022-06-02 00:10:45 -0300

    commit aula 4  - Utilizando o interceptador do http
    aula 4  - Utilizando o interceptador do http ( importado e configurado o http_interceptor)

--- Flutter_com_WEB_API/bytebank/lib/http/webclient.dart
+++ Flutter_com_WEB_API/bytebank/lib/http/webclient.dart
@@ -1,7 +1,35 @@
+import 'dart:io';
+
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    //print(data.toString());
+    print('Request-------------');
+    print('Url: ${data.baseUrl}');
+    print('Headers: ${data.headers}');
+    print('Body: ${data.body}');
+
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    //print(data.toString());
+    print('Response------------');
+    print('Status Code: ${data.statusCode}');
+    print('Headers: ${data.headers}');
+    print('Body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
+  final Client client = InterceptedClient.build(interceptors: [
+    LoggingInterceptor(),
+  ]);
   final Response response =
-      await get(Uri.parse('http://192.168.3.107:8080/transactions'));
-  print(response.body);
+      await client.get(Uri.parse('http://192.168.3.107:8080/transactions'));
 }
