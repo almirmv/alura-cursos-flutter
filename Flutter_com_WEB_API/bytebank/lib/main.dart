7306ce5964365fede31bbd3a4453f90a73cc4a4b
almirmv <76816905+almirmv@users.noreply.github.com>
2022-06-03 19:46:16 -0300

    commit aula 9 - Salvando Transferências
    commit aula 9 - Salvando Transferências. Implementado o método save() para efetuar um POST de uma transaction na WEB API. Não foi implementado tratamento de erros de requisição.

--- Flutter_com_WEB_API/bytebank/lib/main.dart
+++ Flutter_com_WEB_API/bytebank/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bytebank/http/webclient.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const BytebankApp());
+  save(Transaction(200.00, Contact(0, 'Gui', 2407)))
+      .then((transaction) => print(transaction));
   //findAll().then((transactions) => print('new transactions $transactions'));
 }
 
