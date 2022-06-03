1da600ca09e02ba0a608f227caaeb03eeabcc8bc
almirmv <76816905+almirmv@users.noreply.github.com>
2022-06-03 16:00:09 -0300

    commit aula 7 - carregando lista de transferencias na tela
    commit aula 7 - carregando lista de transferências na tela. Implementado também um componente reaproveitável de "Loading"

--- Flutter_com_WEB_API/bytebank/lib/screens/transactions_list.dart
+++ Flutter_com_WEB_API/bytebank/lib/screens/transactions_list.dart
@@ -1,11 +1,12 @@
-import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/components/progress.dart';
+import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = List.empty(growable: true);
+  const TransactionsList({Key? key}) : super(key: key);
 
-  TransactionsList({Key? key}) : super(key: key);
+  //TransactionsList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,29 +15,48 @@ class TransactionsList extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Transactions'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: const Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: const TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+      body: FutureBuilder<List<Transaction>>(
+        //future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        future: findAll(),
+        builder: (context, snapshot) {
+          final List<Transaction>? transactions = snapshot.data;
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Progress();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Transaction transaction = transactions![index];
+                  return Card(
+                    child: ListTile(
+                      leading: const Icon(Icons.monetization_on),
+                      title: Text(
+                        transaction.value.toString(),
+                        style: const TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        transaction.contact.accountNumber.toString(),
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: transactions!.length,
+              );
+              break;
+          }
+          return Text('Unknown error');
         },
-        itemCount: transactions.length,
       ),
     );
   }
