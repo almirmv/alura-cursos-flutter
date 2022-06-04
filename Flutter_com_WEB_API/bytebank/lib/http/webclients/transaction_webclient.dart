75bc2f236c5083af92c9c90a50dd20ac16fb7237
almirmv <76816905+almirmv@users.noreply.github.com>
2022-06-03 22:24:43 -0300

    commit aula 12 - serializando JSON no objeto
    commit aula 12 - serializando JSON no objeto. Implementado extra aula timeout na função save(), dessa forma lidandocom erro de comunicação durante um save.

--- Flutter_com_WEB_API/bytebank/lib/http/webclients/transaction_webclient.dart
+++ Flutter_com_WEB_API/bytebank/lib/http/webclients/transaction_webclient.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:bytebank/http/webclient.dart';
-import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart' as http;
 
@@ -20,23 +19,31 @@ class TransactionWebClient {
     if (response.statusCode == 200) {
       return _toTransactions(response);
     }
-    throw ("Connection error!"); // error thrown
+    throw ("Connection error: findAll()"); // error thrown
   }
 
   //Salvando uma transaction via WEB API
   Future<Transaction> save(Transaction transaction) async {
-    Map<String, dynamic> transactionMap = _toMap(transaction);
-    final String transactionJson = jsonEncode(transactionMap);
+    final String transactionJson = jsonEncode(transaction.toJson());
 
-    final http.Response response =
-        await http.post(Uri.parse('$baseUrl/transactions'),
+    final http.Response response = await client
+        .post(Uri.parse('$baseUrl/transactions'),
             headers: {
               'content-type': 'application/json',
               'password': '1000',
             },
-            body: transactionJson);
-
-    return _toTransaction(response);
+            body: transactionJson)
+        .timeout(
+      const Duration(seconds: 5),
+      onTimeout: () {
+        return http.Response(
+            'Error', 408); // Request Timeout response status code
+      },
+    );
+    if (response.statusCode == 200) {
+      return _toTransaction(response);
+    }
+    throw ("Connection error: save()"); // error thrown
   }
 
   List<Transaction> _toTransactions(http.Response response) {
@@ -44,16 +51,7 @@ class TransactionWebClient {
     final List<dynamic> decodedJson = jsonDecode(response.body);
 
     for (Map<String, dynamic> transactionJson in decodedJson) {
-      final Map<String, dynamic> contactJson = transactionJson['contact'];
-      final Transaction transaction = Transaction(
-        transactionJson['value'],
-        Contact(
-          0,
-          contactJson['name'],
-          contactJson['accountNumber'],
-        ),
-      );
-      transactions.add(transaction);
+      transactions.add(Transaction.fromJson(transactionJson));
     }
     return transactions;
   }
@@ -61,27 +59,6 @@ class TransactionWebClient {
   //http response to transaction
   Transaction _toTransaction(http.Response response) {
     Map<String, dynamic> json = jsonDecode(response.body);
-    final Map<String, dynamic> contactJson = json['contact'];
-    //devolve uma Transaction com dados de resposta
-    return Transaction(
-      json['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
-  }
-
-  //transaction to map<string>
-  Map<String, dynamic> _toMap(Transaction transaction) {
-    final Map<String, dynamic> transactionMap = {
-      'value': transaction.value,
-      'contact': {
-        'name': transaction.contact.name,
-        'accountNumber': transaction.contact.accountNumber
-      }
-    };
-    return transactionMap;
+    return Transaction.fromJson(json);
   }
 }
