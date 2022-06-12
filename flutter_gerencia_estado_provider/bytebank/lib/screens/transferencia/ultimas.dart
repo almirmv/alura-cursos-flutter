6dec0a2978a2861785538b91bc055771c3137063
almirmv <76816905+almirmv@users.noreply.github.com>
2022-06-12 19:59:30 -0300

    commit aula 15 - Validações finais
    commit aula 15 - Validações finais

--- flutter_gerencia_estado_provider/bytebank/lib/screens/transferencia/ultimas.dart
+++ flutter_gerencia_estado_provider/bytebank/lib/screens/transferencia/ultimas.dart
@@ -16,12 +16,24 @@ class UltimasTransferencias extends StatelessWidget {
           style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
         ),
         Consumer<Transferencias>(builder: (context, transferencias, child) {
+          final _ultimasTransferencias =
+              transferencias.transferencias.reversed.toList();
+          final _quantidade = transferencias.transferencias.length;
+          int tamanho = 2;
+
+          if (_quantidade == 0) {
+            return SemTransferenciaCadastrada();
+          }
+
+          if (_quantidade < 2) {
+            tamanho = _quantidade;
+          }
           return ListView.builder(
               padding: const EdgeInsets.all(8.0),
-              itemCount: 2,
+              itemCount: tamanho,
               shrinkWrap: true,
               itemBuilder: (context, indice) {
-                return ItemTransferencia(transferencias.transferencias[indice]);
+                return ItemTransferencia(_ultimasTransferencias[indice]);
               });
         }),
         ElevatedButton(
@@ -31,9 +43,27 @@ class UltimasTransferencias extends StatelessWidget {
               return ListaTransferencias();
             }));
           },
-          child: const Text('Transferências'),
+          child: const Text('Ver todas transferências'),
         ),
       ],
+    );
+  }
+}
+
+class SemTransferenciaCadastrada extends StatelessWidget {
+  const SemTransferenciaCadastrada({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      margin: EdgeInsets.all(40),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Você ainda não cadastrou nenhuma Transferência',
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
