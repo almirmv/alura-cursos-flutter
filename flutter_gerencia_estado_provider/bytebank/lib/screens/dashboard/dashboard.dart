f3d57d78912fb8540b2d27de0d57a56fb69a3caf
almirmv <76816905+almirmv@users.noreply.github.com>
2022-06-11 23:11:23 -0300

    commit aula 5 - Adiciona
    Criado método "adiciona" no model saldo, criado botão no dashboard.

--- flutter_gerencia_estado_provider/bytebank/lib/screens/dashboard/dashboard.dart
+++ flutter_gerencia_estado_provider/bytebank/lib/screens/dashboard/dashboard.dart
@@ -1,5 +1,7 @@
+import 'package:bytebank/models/saldo.dart';
 import 'package:bytebank/screens/dashboard/saldoCard.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -10,7 +12,17 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ByteBank'),
       ),
-      body: Align(alignment: Alignment.topCenter, child: SaldoCard()),
+      body: ListView(
+        children: [
+          Align(alignment: Alignment.topCenter, child: SaldoCard()),
+          Consumer<Saldo>(builder: (context, saldo, child) {
+            return ElevatedButton(
+              onPressed: () => {saldo.adiciona(10)},
+              child: const Text('Adiciona'),
+            );
+          }),
+        ],
+      ),
     );
   }
 }
