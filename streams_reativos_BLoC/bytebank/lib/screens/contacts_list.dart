4744835455fe0d1ebe62d23f311bb3b48c7e0ec4
almirmv <76816905+almirmv@users.noreply.github.com>
2022-06-22 18:53:06 -0300

    Aula 7 - Estados, telas e polimorfismo
    convertendo o "contacts_list" para bloc.

--- streams_reativos_BLoC/bytebank/lib/screens/contacts_list.dart
+++ streams_reativos_BLoC/bytebank/lib/screens/contacts_list.dart
@@ -5,68 +5,96 @@ import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path/path.dart';
+
+@immutable
+abstract class ContactsListState {
+  const ContactsListState();
+}
+
+@immutable
+class InitContactsListState extends ContactsListState {
+  const InitContactsListState();
+}
+
+@immutable
+class LoadingContactsListState extends ContactsListState {
+  const LoadingContactsListState();
+}
+
+@immutable
+class LoadedContactsListState extends ContactsListState {
+  final List<Contact> _contacts;
+  const LoadedContactsListState(this._contacts);
+}
+
+@immutable
+class FatalErrorContactsListState extends ContactsListState {
+  const FatalErrorContactsListState();
+}
+
+class ContactsListCubit extends Cubit<ContactsListState> {
+  ContactsListCubit() : super(InitContactsListState());
+
+  void reload(ContactDao dao) async {
+    emit(LoadingContactsListState());
+    dao.findAll().then((contacts) => emit(LoadedContactsListState(contacts)));
+  }
+}
 
 class ContactsListContainer extends BlocContainer {
   @override
   Widget build(BuildContext context) {
-    return ContactsList();
+    final ContactDao dao = ContactDao();
+
+    return BlocProvider<ContactsListCubit>(
+      create: (BuildContext context) {
+        final cubit = ContactsListCubit();
+        cubit.reload(dao);
+        return cubit;
+      },
+      child: ContactsList(dao),
+    );
   }
 }
 
-class ContactsList extends StatefulWidget {
-  ContactsList({Key? key}) : super(key: key);
+class ContactsList extends StatelessWidget {
+  final ContactDao _dao;
+  const ContactsList(this._dao);
 
-  final ContactDao _dao = ContactDao();
-  @override
-  State<ContactsList> createState() => _ContactsListState();
-}
-
-class _ContactsListState extends State<ContactsList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Transfer'),
       ),
-      //indicando que o future vai receber uma <list<Contact>> (generics)
-      body: FutureBuilder<List<Contact>>(
-        future: widget._dao.findAll(), //faz busca dos contacts
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              break;
-            case ConnectionState.waiting:
-              return Progress();
-              break;
-            case ConnectionState.active:
-              //Ainda fazendo loading mas ja temos dados...
-              break;
-            case ConnectionState.done:
-              //checagem para evitar "null exception"
-              if (snapshot.hasData) {
-                final contacts = snapshot.data!;
-                return ListView.builder(
-                  itemBuilder: (context, index) {
-                    final Contact contact = contacts[index];
-                    return _ContactItem(
-                      contact,
-                      onClick: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => TransactionForm(contact),
-                          ),
-                        );
-                      },
+      body: BlocBuilder<ContactsListCubit, ContactsListState>(
+        builder: (context, state) {
+          if (state is InitContactsListState ||
+              state is LoadingContactsListState) {
+            return Progress();
+          }
+          if (state is LoadedContactsListState) {
+            final contacts = state._contacts;
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                final Contact contact = contacts[index];
+                return _ContactItem(
+                  contact,
+                  onClick: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => TransactionForm(contact),
+                      ),
                     );
                   },
-                  itemCount: contacts.length,
                 );
-              }
-              return const Text('No contacts...');
-              break;
+              },
+              itemCount: contacts.length,
+            );
           }
-          //mesmo que nunca chegue aqui o dart exige um retorno...
-          return const Text('Unknown Error');
+          return const Text('No contacts...');
         },
       ),
       floatingActionButton: buildAddContactButton(context),
@@ -75,25 +103,17 @@ class _ContactsListState extends State<ContactsList> {
 
   FloatingActionButton buildAddContactButton(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {
-        Navigator.of(context)
-            .push(
-              MaterialPageRoute(
-                builder: (context) => const ContactForm(),
-              ),
-            )
-            .then(
-              (value) => update(),
-            );
+      onPressed: () async {
+        await Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ContactForm(),
+          ),
+        );
+        //quando a navegaçao voltar chama o reload
+        context.read<ContactsListCubit>().reload(_dao);
       },
       child: const Icon(Icons.add),
     );
-  }
-
-//nojento, pois não temos estado e não estamos usando o Bloc e esta horrivel
-//melhoraremos
-  update() {
-    setState(() {});
   }
 }
 
